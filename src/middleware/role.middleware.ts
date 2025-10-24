e5f206a95a48fd2c464cbe9d4eb3a86d786a4b02
verwann <erwann.varlet@ynov.com>
2025-10-24 13:49:12 +0200

    add user.role and create middleware

---
+++ src/middleware/role.middleware.ts
@@ -0,0 +1,21 @@
+﻿import type {NextFunction, Request, Response} from "express";
+import db from "../config/db";
+
+export const admin =
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const userId = (req as any).user;
+            const user = await db.user.findFirst({
+                where: {id: userId},
+                select: {role: true}
+            })
+            if (user?.role === 'ADMIN') return next();
+
+            return res.status(403).json({
+                error: 'ADMIN role required',
+            });
+
+        } catch (error) {
+            next(error);
+        }
+    }
