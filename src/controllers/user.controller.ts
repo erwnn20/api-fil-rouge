4a6f0b32a6032a4d67feef35c3bd9ae1060da8d7
verwann <erwann.varlet@ynov.com>
2025-10-24 23:30:47 +0200

    add User type, update json responses

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,94 +1,112 @@
 ﻿import type {Request, Response, NextFunction} from 'express';
 import * as pwd from "../utils/password.utils";
 import db from "../config/db";
+import {User as PrismaUser} from "@prisma/client";
 
-export const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const data = req.body;
-        const user = await db.user.create({
-            data: {
-                firstname: data.firstname,
-                lastname: data.lastname,
-                username: data.username,
-                email: data.email,
-                password: pwd.hash(data.password),
-            },
-        });
 
-        res.json({
-            message: `User created successfully.`,
-            user: user,
-        });
-    } catch (error) {
-        next(error);
-    }
-};
+export type User = Omit<PrismaUser, "password">;
 
-export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const data = req.query;
-        const users = await db.user.findMany({
-            where: {
-                id: req.params.id ? Number(req.params.id) : undefined,
-                firstname: data.firstname as string | undefined,
-                lastname: data.lastname as string | undefined,
-                username: data.username as string | undefined,
-                email: data.email as string | undefined,
-                // created_at: Date,
-            },
-        })
+export const createUser =
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data = req.body;
+            const user: User = await db.user.create({
+                data: {
+                    firstname: data.firstname,
+                    lastname: data.lastname,
+                    username: data.username,
+                    email: data.email,
+                    password: pwd.hash(data.password),
+                },
+                omit: {
+                    password: true
+                }
+            });
 
-        res.json({
-            message: users.length === 0 ? 'no User found.' : `${users.length} Users found.`,
-            users: users,
-            count: users.length,
-        });
-    } catch (error) {
-        next(error);
-    }
-};
+            res.json({
+                message: `User \`${user.username}\` created successfully`, user,
+            });
+        } catch (error) {
+            next(error);
+        }
+    };
 
-export const updateUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const data = req.body;
-        const user = await db.user.update({
-            where: {
-                id: req.params.id ? Number(req.params.id) : undefined,
-            },
-            data: {
-                firstname: data.firstname,
-                lastname: data.lastname,
-                username: data.username,
-                email: data.email,
-            },
-        })
+export const getUsers =
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data = req.query;
+            const users: User[] = await db.user.findMany({
+                where: {
+                    id: req.params.id ? Number(req.params.id) : undefined,
+                    firstname: data.firstname as string | undefined,
+                    lastname: data.lastname as string | undefined,
+                    username: data.username as string | undefined,
+                    email: data.email as string | undefined,
+                    // created_at: Date,
+                },
+                omit: {
+                    password: true
+                }
+            });
 
-        res.json({
-            message: `User updated successfully.`,
-            user: user,
-            updated: {
-                firstname: data.firstname,
-                lastname: data.lastname,
-                username: data.username,
-                email: data.email,
-            },
-        });
-    } catch (error) {
-        next(error);
-    }
-};
+            res.json({
+                message: users.length === 0 ? 'No user found' : `${users.length} users found`,
+                users, count: users.length,
+            });
+        } catch (error) {
+            next(error);
+        }
+    };
 
-export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        await db.user.delete({
-            where: {
-                id: req.params.id ? Number(req.params.id) : undefined
-            },
-        });
-        res.json({
-            message: `User deleted successfully.`,
-        });
-    } catch (error) {
-        next(error);
-    }
-};
+export const updateUser =
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data = req.body;
+            const user: User = await db.user.update({
+                where: {
+                    id: req.params.id ? Number(req.params.id) : undefined,
+                },
+                data: {
+                    firstname: data.firstname,
+                    lastname: data.lastname,
+                    username: data.username,
+                    email: data.email,
+                },
+                omit: {
+                    password: true
+                }
+            })
+
+            res.json({
+                message: `User \`${user.username}\` updated successfully`, user,
+                updated: {
+                    firstname: data.firstname,
+                    lastname: data.lastname,
+                    username: data.username,
+                    email: data.email,
+                },
+            });
+        } catch (error) {
+            next(error);
+        }
+    };
+
+export const deleteUser =
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const user = await db.user.delete({
+                where: {
+                    id: req.params.id ? Number(req.params.id) : undefined
+                },
+                select: {
+                    username: true
+                }
+            });
+
+            res.json({
+                message: `User \`${user.username}\` deleted successfully`,
+            });
+        } catch (error) {
+            next(error);
+        }
+    };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,9 +5,9 @@ import * as mwRole from "../middleware/role.middleware";
 
 const userRoutes = Router();
 
-userRoutes.post('/', mwAuth.auth, mwRole.required("ADMIN"), createUser);
 userRoutes.get('/', mwAuth.auth, getUsers);
 userRoutes.get('/:id', mwAuth.auth, getUsers);
+userRoutes.post('/', mwAuth.auth, mwRole.required("ADMIN"), createUser);
 userRoutes.put('/:id', mwAuth.auth, mwRole.required("ADMIN"), updateUser);
 userRoutes.delete('/:id', mwAuth.auth, mwRole.required("ADMIN"), deleteUser);
 
