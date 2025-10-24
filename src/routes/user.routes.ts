75c5567f048140a6d5fc6fd38eec4183d3ada2fd
verwann <erwann.varlet@ynov.com>
2025-10-24 13:51:55 +0200

    add middleware to user api

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,14 @@
 ﻿import {Router} from "express";
 import {createUser, deleteUser, getUsers, updateUser} from "../controllers/user.controller";
+import * as mwAuth from "../middleware/auth.middleware";
+import * as mwRole from "../middleware/role.middleware";
 
 const userRoutes = Router();
 
-userRoutes.post('/', createUser);
-userRoutes.get('/', getUsers);
-userRoutes.get('/:id', getUsers);
-userRoutes.put('/:id', updateUser);
-userRoutes.delete('/:id', deleteUser);
+userRoutes.post('/', mwAuth.auth, mwRole.admin, createUser);
+userRoutes.get('/', mwAuth.auth, getUsers);
+userRoutes.get('/:id', mwAuth.auth, getUsers);
+userRoutes.put('/:id', mwAuth.auth, mwRole.admin, updateUser);
+userRoutes.delete('/:id', mwAuth.auth, mwRole.admin, deleteUser);
 
 export default userRoutes;
