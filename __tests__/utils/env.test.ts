2654dc1938fb3e50093b52090af47c3201998bab
verwann <erwann.varlet@ynov.com>
2025-11-13 19:09:58 +0100

    implement env utils tests

---
+++ __tests__/utils/env.test.ts
@@ -0,0 +1,28 @@
+﻿import { getEnv } from '../../src/utils/env.utils';
+
+describe('method getEnv', () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+        process.env = { ...OLD_ENV }; // avoid contaminating other tests
+    });
+    afterEach(() => {
+        process.env = OLD_ENV; // restores the original env
+    });
+
+    test('should return the value when the environment variable exists', () => {
+        process.env.TEST_KEY = 'test_value';
+
+        const result = getEnv('TEST_KEY');
+
+        expect(result).toBe('test_value');
+    });
+
+    test('should throw an error when the environment variable is missing', () => {
+        delete process.env.MISSING_KEY;
+
+        expect(() => getEnv('MISSING_KEY')).toThrow(
+            'Missing environment variable: MISSING_KEY'
+        );
+    });
+});
