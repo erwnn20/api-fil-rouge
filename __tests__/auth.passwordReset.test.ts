061b594470180475d9f283456434a8578f1cd4ea
verwann <erwann.varlet@ynov.com>
2025-11-13 18:38:12 +0100

    implement POST /auth/password-reset route tests

---
+++ __tests__/auth.passwordReset.test.ts
@@ -0,0 +1,23 @@
+﻿import request from 'supertest';
+import * as jwt from '../src/utils/jwt.utils';
+import app from "../src/app";
+
+import * as middleware from '../src/middleware/middleware.tests'
+
+
+const method = 'POST';
+const route = '/auth/password-reset'
+
+describe(`${method} ${route}`, () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should register a new user successfully', async () => {
+        const response = await request(app)
+            .post(route)
+            .expect(501);
+
+        expect(response.body.error).toBe( 'Not implemented');
+    });
+});
