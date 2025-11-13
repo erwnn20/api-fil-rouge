a60eb62d7c8c86c755cf9bc3eb8ec33316dd7913
verwann <erwann.varlet@ynov.com>
2025-11-13 12:13:00 +0100

    implement POST /auth/register route tests

---
+++ __tests__/auth.register.test.ts
@@ -0,0 +1,87 @@
+﻿import request from 'supertest';
+import * as jwt from '../src/utils/jwt.utils';
+import * as pwd from '../src/utils/password.utils';
+import {dbMock} from "../src/config/singleton";
+import app from "../src/app";
+import db from "../src/config/db";
+
+import * as middleware from '../src/middleware/middleware.tests'
+
+
+const method = 'POST';
+const route = '/auth/register'
+
+describe(`${method} ${route}`, () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    middleware.guest(route);
+
+    const fakeUser = {
+        username: 'fakeUser',
+        email: 'fake.user@mail.com',
+        firstname: 'User',
+        lastname: 'Fake',
+        password: 'password',
+    };
+
+    it('should return 401 if email is already used', async () => {
+        dbMock.user.count.mockResolvedValueOnce(1); // email already used
+
+        const response = await request(app)
+            .post(route)
+            .send(fakeUser)
+            .expect(401);
+
+        expect(response.body.error).toBe('Email already used');
+    });
+    it('should return 401 if email is already used', async () => {
+        dbMock.user.count
+            .mockResolvedValueOnce(0) // email ok
+            .mockResolvedValueOnce(1); // username already used
+
+        const response = await request(app)
+            .post(route)
+            .send(fakeUser)
+            .expect(401);
+
+        expect(response.body.error).toBe('Username already used');
+    });
+
+    it('should register a new user successfully', async () => {
+        dbMock.user.count.mockResolvedValue(0); // email + username not used
+        dbMock.user.create.mockResolvedValue({
+            id: 1,
+            username: fakeUser.username,
+        } as any);
+
+        jest.spyOn(pwd, 'hash').mockReturnValue('hashed_password');
+        jest.spyOn(jwt, 'generate').mockResolvedValue({
+            access: 'fake_access_token',
+            refresh: 'fake_refresh_token',
+        });
+
+        const response = await request(app)
+            .post(route)
+            .send(fakeUser)
+            .expect(201);
+
+        expect(db.user.count).toHaveBeenCalledTimes(2);
+        expect(db.user.create).toHaveBeenCalledWith({
+            data: expect.objectContaining({
+                username: fakeUser.username,
+                email: fakeUser.email,
+                firstname: fakeUser.firstname,
+                lastname: fakeUser.lastname,
+                password: 'hashed_password',
+            }),
+            select: {id: true, username: true},
+        });
+        expect(response.body).toEqual({
+            message: `User \`${fakeUser.username}\` registered successfully`,
+            accessToken: 'fake_access_token',
+        });
+        expect(response.headers['set-cookie'][0]).toContain('refreshToken=fake_refresh_token');
+    });
+});
