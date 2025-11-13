db697ceaeee8f45a5be9a483ccfc51f820991afd
verwann <erwann.varlet@ynov.com>
2025-11-13 11:01:49 +0100

    implement db mock

---
+++ src/config/singleton.ts
@@ -0,0 +1,16 @@
+﻿import {PrismaClient} from '@prisma/client'
+import {mockDeep, mockReset, DeepMockProxy} from 'jest-mock-extended'
+
+import db from './db'
+
+
+jest.mock('./db', () => ({
+    __esModule: true,
+    default: mockDeep<PrismaClient>(),
+}))
+
+beforeEach(() => {
+    mockReset(dbMock)
+})
+
+export const dbMock = db as unknown as DeepMockProxy<PrismaClient>;
