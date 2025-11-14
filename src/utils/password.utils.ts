3a83bb7f054defcbb184cd36ed23e8751826bdb5
verwann <erwann.varlet@ynov.com>
2025-11-14 21:06:57 +0100

    pass

--- src/utils/password.utils.ts
+++ src/utils/password.utils.ts
@@ -3,4 +3,5 @@
 const BCRYPT_ROUNDS = Number(process.env.BCRYPT_ROUNDS) || 12;
 
 export const hash = (password: string) => bcrypt.hashSync(password, BCRYPT_ROUNDS)
-export const compare = (password: string, encrypted: string) => bcrypt.compareSync(password, encrypted)
+
+export const compare = (password: string, encrypted: string) => bcrypt.compareSync(password, encrypted);
