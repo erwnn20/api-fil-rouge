362f8492e6fc8c37dae2fe3239b533639de904b8
verwann <erwann.varlet@ynov.com>
2025-11-16 19:28:39 +0100

    implements middleware to check requests body with zod

---
+++ src/middleware/zod.middleware.ts
@@ -0,0 +1,18 @@
+﻿import {z} from "zod";
+import type {NextFunction, Request, Response} from "express";
+
+export const validateBody =
+    (schema: z.ZodSchema<any>) =>
+        (req: Request, res: Response, next: NextFunction) => {
+            const result = schema.safeParse(req.body);
+
+            if (!result.success) {
+                return res.status(400).json({
+                    error: 'Invalid request body',
+                    details: z.flattenError(result.error),
+                });
+            }
+
+            req.body = result.data;
+            next();
+        };
